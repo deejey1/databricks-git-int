4f3fc30ba8a00cb14a887987bc3bb965b0aab580
Divyansh Jain <divyanshjain837@gmail.com>
2021-08-20 16:42:18 +0530

    sss1111

--- notebooks/demo.py
+++ notebooks/demo.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
-df2 = spark.createDataFrame([('Ankit',25),('Jalfaizy',22),('saurabh',20),('Bala',26)])
+df3 = spark.createDataFrame([('Ankit',25),('Jalfaizy',22),('saurabh',20),('Bala',26)])
 
 # COMMAND ----------
 
-display(df2)
+display(df3)
